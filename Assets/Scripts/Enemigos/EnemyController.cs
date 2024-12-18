b7b9ae343e3d72d0eb11e4fffd5ba10ca5a66708
Sammmplay <sammmplay@outlook.com>
2024-12-18 00:09:31 +0000

    18/12 Trabajo

--- Assets/Scripts/Enemigos/EnemyController.cs
+++ Assets/Scripts/Enemigos/EnemyController.cs
@@ -5,10 +5,21 @@ using UnityEngine.UI;
 
 public class EnemyController : MonoBehaviour
 {
+    public static EnemyController instance;
     public float _life;
     public float _maxLife;
+    float _timeNextFire;
+    [SerializeField] float _timeInstanceBulletMax;
+    [SerializeField] float _timeInstanceBulletMin;
+    [SerializeField] GameObject _prefabProyectil;
+    [SerializeField] float _distanceRaycast;
     Slider _slider;
     [SerializeField] Canvas _canvas;
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else Destroy(gameObject);
+    }
     private void Start() {
         _life = _maxLife;
         _slider = GetComponentInChildren<Slider>();
@@ -17,6 +28,13 @@ public class EnemyController : MonoBehaviour
             _canvas.worldCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         }
         InicializarVida(_life,_maxLife);
+        _timeNextFire = Random.Range(_timeInstanceBulletMin, _timeInstanceBulletMax);
+    }
+
+    private void Update() {
+        if (!PuedeAtaquar()) return;
+            Disparos();
+            
     }
 
     public void InicializarVida(float lifeactual, float maxlife) {
@@ -30,5 +48,23 @@ public class EnemyController : MonoBehaviour
         if(_life<= 0) {
             Destroy(gameObject);
         }
+    }
+    void Disparos() {
+        _timeNextFire -= Time.deltaTime;
+        if(_timeNextFire <= 0) {
+            InstanciarProyectiil();
+            _timeNextFire = Random.Range(_timeInstanceBulletMin, _timeInstanceBulletMax);
+        }
+    }
+    void InstanciarProyectiil () {
+        GameObject _instanciaProyectil = Instantiate(_prefabProyectil);
+    }
+    bool PuedeAtaquar() {
+
+        if (Physics.Raycast(transform.position, Vector3.back, out RaycastHit hit, _distanceRaycast)) {
+            return !hit.collider.CompareTag("Enemigo");
+
+        }
+        return true;
     }
 }
