6aea032eb75d1432947b5ef513d035eba9c0e12a
Sammmplay <sammmplay@outlook.com>
2024-12-22 18:49:34 +0000

    22/12 Casa
    animacion del shield ajustando los parametros del efect graph
    empezando con los ajustes para las coliciones con el bunker

--- Assets/Scripts/Player/LifeBunker.cs
+++ Assets/Scripts/Player/LifeBunker.cs
@@ -8,9 +8,23 @@ public class LifeBunker : MonoBehaviour
     [SerializeField] float _lifemaxBunker;
     public float _lifeBunker;
     public Slider _sliderLife;
+    [SerializeField] Animator _anim;
+
+    public float fresnelPower;
+    public float frecuency;
     private void Start() {
         _sliderLife.maxValue = _lifemaxBunker;
         _sliderLife.value = _lifemaxBunker;
+        if (_anim == null) {
+            _anim = GetComponentInChildren<Animator>();
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other) {
+        
+    }
+    public void AnimacionGolpeShield() {
+        _anim.SetTrigger("Golpe");
     }
     public void PerderVida(float damage) {
 
